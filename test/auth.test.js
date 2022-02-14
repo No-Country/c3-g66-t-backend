8e4d436a5559351a8b4637fd4c731e9fcb9086e4
Brian Delgado <braiandelgado4255@hotmail.com>
2022-02-13 22:29:18 -0300

    Login tests added

--- test/auth.test.js
+++ test/auth.test.js
@@ -15,20 +15,56 @@ afterAll(() => {
   connection.disconnect();
 });
 
-describe("USER AUTH/REGISTER", () => {
-  test("headers", async () => {
+describe("REGISTER", () => {
+  test("HEADERS", async () => {
     await server
       .post(`${api.prefix}/auth/register`)
       .send(initialUsers[0])
       .expect("Content-Type", /application\/json/)
       .expect(201);
   });
-  test("body", async () => {
-    const res = await server.post(`${api.prefix}/auth/register`);
-    expect(res.body.data.firstname).toContain("John");
-    expect(res.body.data.lastname).toContain("Doe");
-    expect(res.body.data.dni).toContain(12345678);
-    expect(res.body.data.email).toContain("john@doe.com");
-    expect(res.body.data.phone).toContain("+541101234567");
+  test("BODY", async () => {
+    const res = await server
+      .post(`${api.prefix}/auth/register`)
+      .send(initialUsers[0]);
+    expect(res.body.data.firstname).toBe("John");
+    expect(res.body.data.lastname).toBe("Doe");
+    expect(res.body.data.dni).toBe("12345678");
+    expect(res.body.data.email).toBe("john@doe.com");
+    expect(res.body.data.phone).toBe("+541101234567");
+    expect(res.body.data.img).toBe(
+      "https://i.blogs.es/b54943/god1/840_560.jpg"
+    );
+    expect(res.body.data.password).not.toBeDefined();
+    expect(res.body.data.token).toBeDefined();
+  });
+});
+
+describe("LOGIN", () => {
+  test("HEADERS", async () => {
+    await server.post(`${api.prefix}/auth/register`).send(initialUsers[0]);
+    const { email, password } = initialUsers[0];
+    await server
+      .post(`${api.prefix}/auth/login`)
+      .send({ email, password })
+      .expect("Content-Type", /application\/json/)
+      .expect(200);
+  });
+
+  test("BODY", async () => {
+    await server.post(`${api.prefix}/auth/register`).send(initialUsers[0]);
+    const { email, password } = initialUsers[0];
+    const res = await server
+      .post(`${api.prefix}/auth/login`)
+      .send({ email, password });
+    expect(res.body.data.firstname).toBe("John");
+    expect(res.body.data.lastname).toBe("Doe");
+    expect(res.body.data.dni).toBe("12345678");
+    expect(res.body.data.email).toBe("john@doe.com");
+    expect(res.body.data.phone).toBe("+541101234567");
+    expect(res.body.data.img).toBe(
+      "https://i.blogs.es/b54943/god1/840_560.jpg"
+    );
+    expect(res.body.data.password).not.toBeDefined();
   });
 });
