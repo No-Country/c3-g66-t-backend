b74621ebb37e90b70408fbdda058901d61f3ea58
Brian Delgado <braiandelgado4255@hotmail.com>
2022-02-13 22:30:36 -0300

    login validations added

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,9 +1,12 @@
 const { Router } = require("express");
-const { registerValidations } = require("../middlewares/validations/Auth");
-const { register } = require("../controllers/auth");
+const {
+  registerValidations,
+  loginValidations,
+} = require("../middlewares/validations/Auth");
+const { register, login } = require("../controllers/auth");
 
 const router = Router();
-router.route("/login");
+router.route("/login").post(loginValidations, login);
 router.route("/register").post(registerValidations, register);
 
 module.exports = router;
