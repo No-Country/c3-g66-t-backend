c1691463ce7961946c1efa8deba319a110fa58cf
Brian Delgado <braiandelgado4255@hotmail.com>
2022-02-17 13:45:28 -0300

    return formated data

--- src/services/Location.js
+++ src/services/Location.js
@@ -1,5 +1,6 @@
 const { default: axios } = require("axios");
 const { mapbox } = require("../config");
+const ErrorResponse = require("../helpers/ErrorResponse");
 
 class LocationService {
   constructor() {
@@ -8,8 +9,27 @@ class LocationService {
   }
 
   async search(keyword) {
-    const res = await axios.get(`${this.url}${keyword}${this.apiKey}`);
-    console.log(res.data);
+    try {
+      const querys = "&language=es";
+      const res = await axios.get(
+        `${this.url}${keyword}.json${this.apiKey}${querys}`
+      );
+      return res.data.features.map(e => {
+        return {
+          _id: e.id,
+          place_name: e.place_name,
+          text: e.text,
+          latitude: e.geometry.coordinates[1],
+          longitude: e.geometry.coordinates[0],
+        };
+      });
+    } catch (error) {
+      throw new ErrorResponse(
+        error.response.status,
+        undefined,
+        error.response.statusText
+      );
+    }
   }
 }
 
