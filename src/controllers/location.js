fbf5925caf019ad4f383a6f6e4ca5fd103aa139b
Brian Delgado <braiandelgado4255@hotmail.com>
2022-02-17 13:45:03 -0300

    data response

--- src/controllers/location.js
+++ src/controllers/location.js
@@ -5,7 +5,7 @@ const LocationService = require("../services/Location");
 exports.getLocation = async (req, res, next) => {
   try {
     const data = await LocationService.search(req.params.location);
-    res.status(200).json(new Success(200, "Locations Finded", "data"));
+    res.status(200).json(new Success(200, "Locations Finded", data));
   } catch (error) {
     next(
       new ErrorResponse(
