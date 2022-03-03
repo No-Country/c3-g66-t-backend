f87b106d3641aa9cba9445c15d3e6b2f746ac5b3
Brian Delgado <braiandelgado4255@hotmail.com>
2022-03-03 12:34:50 -0300

    Default values for required query params added

--- src/services/Hotel.js
+++ src/services/Hotel.js
@@ -4,25 +4,25 @@ const axios = require("axios").default;
 class HotelService {
   constructor(params) {
     // prettier-ignore
-    this._url = "https://hotels-com-provider.p.rapidapi.com/v1/hotels/",
+    this._url = "https://hotels-com-provider.p.rapidapi.com/v1/hotels",
     this._querys = {
       latitude: params.latitude,
       longitude: params.longitude,
       checkin_date: params.checkin_date,
       checkout_date: params.checkout_date,
-      sort_order: params.sort_order,
+      sort_order: params.sort_order || "STAR_RATING_HIGHEST_FIRST",
       price_max: params.price_max,
       price_min: params.price_min,
       star_rating_ids: params.star_rating_ids,
-      adults_number: params.adults_number,
+      adults_number: params.adults_number || "1",
       children_ages: params.children_ages,
       amenity_ids: params.amenity_ids,
       theme_ids: params.theme_ids,
       accommodation_ids: params.accommodation_ids,
       guest_rating_min: params.guest_rating_min,
-      page_number: params.page_number,
-      currency: params.currency,
-      locale: params.locale,
+      page_number: params.page_number || "1",
+      currency: params.currency || "USD",
+      locale: params.locale || "es_ES",
       hotel_id: params.hotel_id,
     };
 
@@ -38,7 +38,7 @@ class HotelService {
   async getList() {
     try {
       const res = await axios.get(
-        this._url + "nearby?" + this._stringifyQuery,
+        this._url + "/nearby?" + this._stringifyQuery,
         {
           headers: { "x-rapidapi-key": process.env.API_KEY },
         }
