621c2582845c176dba79429c8fdf74ef8d9c39ae
Brian Delgado <braiandelgado4255@hotmail.com>
2022-03-03 17:04:04 -0300

    list validations added

--- src/routes/hotel.js
+++ src/routes/hotel.js
@@ -1,10 +1,10 @@
 const { Router } = require("express");
 const { list, photos, reviews } = require("../controllers/hotel");
-//const { hotelListValidation } = require("../middlewares/validations/Hotel");
+const { hotelListValidation } = require("../middlewares/validations/Hotel");
 
 const router = Router();
 
-router.route("/list").get(list);
+router.route("/list").get(hotelListValidation, list);
 router.route("/photos").get(photos);
 router.route("/reviews").get(reviews);
 
