03ca443726cc2466baddb8fb124816c0502cb0df
Brian Delgado <braiandelgado4255@hotmail.com>
2022-03-03 20:02:06 -0300

    routes for get & create reviews

--- src/routes/hotel.js
+++ src/routes/hotel.js
@@ -1,11 +1,26 @@
 const { Router } = require("express");
-const { list, photos, reviews } = require("../controllers/hotel");
-const { hotelListValidation } = require("../middlewares/validations/Hotel");
+const {
+  list,
+  photos,
+  reviews,
+  localReviews,
+  createReview,
+} = require("../controllers/hotel");
+const {
+  hotelListValidation,
+  photosValidations,
+  reviewsValidations,
+  createReviewValidations,
+} = require("../middlewares/validations/Hotel");
 
 const router = Router();
 
 router.route("/list").get(hotelListValidation, list);
-router.route("/photos").get(photos);
-router.route("/reviews").get(reviews);
+router.route("/photos").get(photosValidations, photos);
+router.route("/reviews").get(reviewsValidations, reviews);
+router
+  .route("/localreviews")
+  .get(reviewsValidations, localReviews)
+  .post(createReviewValidations, createReview);
 
 module.exports = router;
